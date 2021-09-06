8565e2fd48081005adc32fd56bb36fa17c806ddd
Ryan Kennel <rkennel@ford.com>
2021-09-06 09:49:45 -0400

    eslint and prettier working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,22 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import {render, RenderResult} from "@testing-library/angular";
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { render, RenderResult } from "@testing-library/angular";
 
-describe('AppComponent', () => {
-  let renderResult:RenderResult<AppComponent>;
+describe("AppComponent", () => {
+  let renderResult: RenderResult<AppComponent>;
   beforeEach(async () => {
     renderResult = await render(AppComponent);
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     expect(renderResult.fixture.componentInstance).toBeTruthy();
   });
 
   it(`should have as title 'angular-starter'`, () => {
-    expect(renderResult.fixture.componentInstance.title).toEqual('angular-starter');
+    expect(renderResult.fixture.componentInstance.title).toEqual("angular-starter");
   });
 
-  it('should render title', () => {
-    expect(renderResult.getByText('angular-starter app is running!'));
+  it("should render title", () => {
+    expect(renderResult.getByText("angular-starter app is running!"));
   });
 });
